c393270fd5acb6620c54339dc933f4705312a3c9
Tony Chia <tchia04@gmail.com>
2017-05-04 16:57:09 -0700

    Revert "test with less than 50mb"
    This reverts commit 6d2bff38dfe7a133137f2522cf43048ef8543a96.

--- setup_pre_commit_hook.sh
+++ setup_pre_commit_hook.sh
@@ -8,14 +8,11 @@ echo -e "\nCreate a directory (~/.git-templates/hooks) to hold the global hoooks
 mkdir -p ~/.git-templates/hooks
 cp pre-commit  ~/.git-templates/hooks
 cp pre-push ~/.git-templates/hooks
-cp protect_default_branch ~/.git-templates/hooks
-cp check_file_size ~/.git-templates/hooks
 
 #DIR="~/" # for production
 #DIR="/tmp/" # for testing
 echo -e "\nFind all the git repos under $DIR directory"
-#ALL_GIT_REPOS=$(find ~/ -name .git  | sed 's/\.git//g')
-ALL_GIT_REPOS=$(find /tmp/ -name .git  | sed 's/\.git//g')
+ALL_GIT_REPOS=$(find ~/ -name .git  | sed 's/\.git//g')
 
 for repo in $ALL_GIT_REPOS; do
   echo -e "\nRunning git init on $repo under $DIR to copy the global hook";
