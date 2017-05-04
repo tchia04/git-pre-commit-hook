91ce25479395fd20ff6b989a0405d091e1474b6e
Tony Chia <tchia04@gmail.com>
2017-05-04 16:58:40 -0700

    test less than 50mb

--- setup_pre_commit_hook.sh
+++ setup_pre_commit_hook.sh
@@ -12,7 +12,8 @@ cp pre-push ~/.git-templates/hooks
 #DIR="~/" # for production
 #DIR="/tmp/" # for testing
 echo -e "\nFind all the git repos under $DIR directory"
-ALL_GIT_REPOS=$(find ~/ -name .git  | sed 's/\.git//g')
+#ALL_GIT_REPOS=$(find ~/ -name .git  | sed 's/\.git//g')
+ALL_GIT_REPOS=$(find /tmp/ -name .git  | sed 's/\.git//g')
 
 for repo in $ALL_GIT_REPOS; do
   echo -e "\nRunning git init on $repo under $DIR to copy the global hook";
